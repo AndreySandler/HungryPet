07185c44e5eb0475732f4d63ba519e4801a10ecd
AndreySandler <mrdreem13@gmail.com>
2022-10-30 19:37:00 +0300

    Some code style fix

--- HungryPet/CatEmotionViewController.swift
+++ HungryPet/CatEmotionViewController.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 class CatEmotionViewController: UIViewController {
+    
+    // MARK: - IBOutlets
     @IBOutlet var catEmotionImageView: UIImageView!
     
+    // MARK: - Public Properties
     var dishType: String!
     
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         catEmotionImageView.image = UIImage(named: "sleeping")
     }
+    
+    // MARK: - IBActions
     @IBAction func feedButtonDidTapped() {
         if dishType == "vegetarian" {
             catEmotionImageView.image = UIImage(named: "sick")
